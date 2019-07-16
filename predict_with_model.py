c119fb79b749dfb36830f6238878784a22c26906
dlsrnsi <dlsrnsladlek@naver.com>
2019-07-16 12:23:25 +0900

    stash change

--- predict_with_model.py
+++ predict_with_model.py
@@ -17,11 +17,17 @@ def encodeSeq(seq, seq_dic):
     else:
         return [seq_dic[aa] for aa in seq]
 
+def encodeSeq(seq, seq_dic):
+    if pd.isnull(seq):
+        return [0]
+    else:
+        return [seq_dic[aa] for aa in seq]
+
 def parse_data(dti_dir, drug_dir, protein_dir, with_label=True,
                prot_len=2500, prot_vec="Convolution",
-               drug_vec="morgan_fp", drug_len=2048):
+               drug_vec="Convolution", drug_len=2048):
 
-    print "Parsing {0} , {1}, {2} with length {3}, type {4}".format(*[dti_dir ,drug_dir, protein_dir, prot_len, prot_vec])
+    print("Parsing {0} , {1}, {2} with length {3}, type {4}".format(*[dti_dir ,drug_dir, protein_dir, prot_len, prot_vec]))
 
     protein_col = "Protein_ID"
     drug_col = "Compound_ID"
@@ -29,30 +35,28 @@ def parse_data(dti_dir, drug_dir, protein_dir, with_label=True,
     if with_label:
         label_col = "Label"
         col_names += [label_col]
-    dti_df = pd.read_csv(dti_dir)#,names=col_names)
-
+    dti_df = pd.read_csv(dti_dir)
     drug_df = pd.read_csv(drug_dir, index_col="Compound_ID")
     protein_df = pd.read_csv(protein_dir, index_col="Protein_ID")
 
-    drug_dic= drug_df[drug_vec].map(lambda fp: fp.split("\t")).to_dict()
-    drug_feature = np.array(list(dti_df[drug_col].map(lambda drug: drug_dic[drug])), dtype=np.float64)
 
-    if prot_vec != "Convolution":
-        prot_dic= protein_df[prot_vec].map(lambda seq: [float(i) for i in seq.split("\t")]).to_dict()
-        protein_feature = np.array(list(dti_df[protein_col].map(lambda protein: prot_dic[protein])), dtype=np.float64)
-
+    if prot_vec == "Convolution":
+        protein_df["encoded_sequence"] = protein_df.Sequence.map(lambda a: encodeSeq(a, seq_dic))
+    dti_df = pd.merge(dti_df, protein_df, left_on=protein_col, right_index=True)
+    dti_df = pd.merge(dti_df, drug_df, left_on=drug_col, right_index=True)
+    drug_feature = np.stack(dti_df[drug_vec].map(lambda fp: fp.split("\t")))
+    if prot_vec=="Convolution":
+        protein_feature = sequence.pad_sequences(dti_df["encoded_sequence"].values, prot_len)
     else:
-        protein_df["seq_num_vec"] = protein_df.Sequence.map(lambda seq: encodeSeq(seq, seq_dic))
-        prot_dic = protein_df.seq_num_vec.to_dict()
-        protein_feature = sequence.pad_sequences(dti_df[protein_col].map(lambda seq:prot_dic[seq]), maxlen=prot_len)
-
+        protein_feature = np.stack(dti_df[prot_vec].map(lambda fp: fp.split("\t")))
     if with_label:
-        label =  dti_df[label_col].values
-        print "\tPositive data : %d" %(sum(dti_df[label_col]))
-        print "\tNegative data : %d" %(dti_df.shape[0] - sum(dti_df[label_col]))
-        return {"protein_feature": protein_feature, "drug_feature": drug_feature, "Label": label}
+        label = dti_df[label_col].values
+        print("\tPositive data : %d" %(sum(dti_df[label_col])))
+        print("\tNegative data : %d" %(dti_df.shape[0] - sum(dti_df[label_col])))
+        return {"protein_feature": protein_feature, "drug_feature": drug_feature, "label": label}
     else:
         return {"protein_feature": protein_feature, "drug_feature": drug_feature}
+
 
 
 if __name__=="__main__":
@@ -63,7 +67,7 @@ if __name__=="__main__":
     parser.add_argument("--test-dti-dir", "-i", help="Test dti [drug, target, [label]]", nargs="*")
     parser.add_argument("--test-drug-dir", "-d", help="Test drug information [drug, SMILES,[feature_name, ..]]", nargs="*")
     parser.add_argument("--test-protein-dir", '-t', help="Test Protein information [protein, seq, [feature_name]]", nargs="*")
-    parser.add_argument("--without-label", "-W", help="Existence of label information in test DTI", action="store_true", default=False)
+    parser.add_argument("--with-label", "-W", help="Existence of label information in test DTI", action="store_true", default=False)
     parser.add_argument("--output", "-o", help="Prediction output", type=str)
     parser.add_argument("--prot-vec", "-v", help="Type of protein feature, if Convolution, it will execute conlvolution on sequeunce", type=str, default="Convolution")
     parser.add_argument("--prot-len", "-l", help="Protein vector length", default=2500, type=int)
@@ -78,7 +82,7 @@ if __name__=="__main__":
     test_proteins = args.test_protein_dir
     test_drugs = args.test_drug_dir
     test_sets = zip(test_names, tests, test_drugs, test_proteins)
-    without_label = not args.without_label
+    with_label = args.with_label
     output_file = args.output
 
 
@@ -87,7 +91,7 @@ if __name__=="__main__":
     try:
         f.__delitem__("optimizer_weights")
     except:
-        print "optimizer_weights are already deleted"
+        print("optimizer_weights are already deleted")
 
     f.close()
 
@@ -97,11 +101,11 @@ if __name__=="__main__":
         "drug_vec": args.drug_vec,
         "drug_len": args.drug_len,
     }
-    test_dic = {test_name: parse_data(test_dti, test_drug, test_protein, with_label=without_label, **type_params)
+    test_dic = {test_name: parse_data(test_dti, test_drug, test_protein, with_label=with_label, **type_params)
                 for test_name, test_dti, test_drug, test_protein in test_sets}
 
     loaded_model = load_model(model)
-    print "prediction"
+    print("prediction")
     result_df = pd.DataFrame()
     result_columns = []
     for dataset in test_dic:
@@ -111,19 +115,16 @@ if __name__=="__main__":
         d_splitted = np.array_split(prediction_dic["drug_feature"], N)
         p_splitted = np.array_split(prediction_dic["protein_feature"], N)
         predicted = sum([np.squeeze(loaded_model.predict([d,p])).tolist() for d,p in zip(d_splitted, p_splitted)], [])
-        #value = predicted[dataset]["predicted"]
-        #value = np.squeeze(value)
-        print dataset+str(len(predicted))
         temp_df[dataset,'predicted'] = predicted
    
-        if without_label:
-           temp_df[dataset, 'label'] = np.squeeze(test_dic[dataset]['Label'])
+        if with_label:
+           temp_df[dataset, 'label'] = np.squeeze(test_dic[dataset]['label'])
         result_df = pd.concat([result_df, temp_df], ignore_index=True, axis=1)
         result_columns.append((dataset, "predicted"))
-        if without_label:
+        if with_label:
            result_columns.append((dataset, "label"))
     result_df.columns = pd.MultiIndex.from_tuples(result_columns)
-    print "save to %s"%output_file
+    print("save to %s"%output_file)
     result_df.to_csv(output_file, index=False)
     '''
     predicted = loaded_model.predict([prediction_dic["drug_feature"],prediction_dic["protein_feature"]])
